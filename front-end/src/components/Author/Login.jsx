04e1f41e9e0ab3c63c7f48f6f3d8311f4dc0a71a
Minh Duc <ducmk254@gmail.com>
2021-03-14 22:57:18 +0700

    fix again htnml change to React component

--- front-end/src/components/Author/Login.jsx
+++ front-end/src/components/Author/Login.jsx
@@ -7,8 +7,8 @@ Login.propTypes = {
 function Login(props) {
     return (
         <form action="" className="author-form">
-            <h2>Enter Your Accou</h2>
-            <div className="error-message">Error: Your Password is not Corre</div>
+            <h2>Enter Your Account</h2>
+            <div className="error-message">Error: Your Password is not Correct</div>
             <input type="email" name="email" id="author-email" required placeholder="Email" />
             <input type="password" name="password" id="author-password" required placeholder="Password"/>
             <button className="btn" type="submit">Login</button>
